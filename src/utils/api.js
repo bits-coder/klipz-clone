e18d44af80c6e6e4248ec9bfcae97df31499e895
Bharani Ganeshan <barunitsolutions@gmail.com>
2023-06-05 14:19:23 +0000

    youtube clone

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const request = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    key: process.env.REACT_APP_API_KEY,
+    key: "AIzaSyCrl2Zqvf6c0aOaUZGH3gfvoLEcnTJdX8U",
   },
 })
